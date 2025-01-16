1f2ca253aaf4ef1f12842f41186b06d193ad264f
Sergey <seregaland@yandex.ru>
2025-01-16 17:09:39 +0300

    fix

--- botcustomer.js
+++ botcustomer.js
@@ -182,7 +182,7 @@ botsupport.on('message', async (msg) => {
                 // сохранить отправленное боту сообщение пользователя в БД
                 const convId = sendMyMessage(str_text, 'text', chatId, messageId, reply_id)
 
-                socket.emit("sendMessageSpec", {
+                socket.emit("sendMessageCustomer", {
                     senderId: chatId,
                     receiverId: chatTelegramId,
                     text: str_text,
